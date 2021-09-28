01df7e237135c010fb546dc9a64f8bcfa7510bd5
Djordje Cvetkovic <djole001@gmail.com>
2021-09-28 10:48:01 +0200

    javacript custommessage

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const $input = document.getElementById('form-email')
 const $error = document.getElementById('error-message')
 
 function toggleVisibility  ($input)  {
-    $input.preventDefault();  
-     $error.classList?.add("show-error")
-   console.log($error)
-} 
-$input.addEventListener('invalid', toggleVisibility)
+$input.preventDefault();  
+if (!$input.originalTarget.value) {
+  $error.innerHTML = "Oops! Please add your email"
+}
+else {
+  $error.innerHTML = "Oops! Please check your email"
+}
+$error.classList?.add("show-error")
+};
 
-
-
+$input.addEventListener('invalid', toggleVisibility);
