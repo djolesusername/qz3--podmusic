c4ff24751b80f569b6da35cb4e9b6e8966b9c3f0
Djordje Cvetkovic <djole001@gmail.com>
2021-09-28 06:59:42 +0200

    desktop version

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("script is working")
+const $input = document.getElementById('form-email')
+const $error = document.getElementById('error-message')
+
+function toggleVisibility  ($input)  {
+    $input.preventDefault();  
+     $error.classList?.add("show-error")
+   console.log($error)
+} 
+$input.addEventListener('invalid', toggleVisibility)
+
+
+
